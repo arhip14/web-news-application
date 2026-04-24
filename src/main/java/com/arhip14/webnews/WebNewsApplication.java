451ada93b3cc0c66c7d900de8d85a93640d2931e
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2026-04-24 18:27:34 +0300

    add structure and maven

---
+++ src/main/java/com/arhip14/webnews/WebNewsApplication.java
@@ -0,0 +1,11 @@
+package com.arhip14.webnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebNewsApplication.class, args);
+    }
+}
